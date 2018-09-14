975f51fc358cd2d1947acfbd2b7e029d63c2baf6
Andrew Lobos <andrew@lobos.me>
2018-09-13 21:45:31 -0400

    it builds!

--- prepare.sh
+++ prepare.sh
@@ -3,7 +3,6 @@
 pushd $(dirname ${BASH_SOURCE}) > /dev/null
 
 REALSENSE_NODE_VERSION=2.0.3
-LIBREALSENSE_VERSION=2.10.2
 
 # realsense2_camera
 rm -rf src/realsense2_camera
@@ -11,13 +10,6 @@ wget https://github.com/intel-ros/realsense/archive/${REALSENSE_NODE_VERSION}.ta
 tar -xf ${REALSENSE_NODE_VERSION}.tar.gz
 mv realsense-${REALSENSE_NODE_VERSION}/realsense2_camera src
 rm -rf realsense-${REALSENSE_NODE_VERSION} ${REALSENSE_NODE_VERSION}.tar.gz
-
-# librealsense
-rm -rf src/librealsense2
-wget https://github.com/IntelRealSense/librealsense/archive/v${LIBREALSENSE_VERSION}.tar.gz
-tar -xf v${LIBREALSENSE_VERSION}.tar.gz
-mv librealsense-${LIBREALSENSE_VERSION} src/librealsense2
-rm -rf librealsense-${LIBREALSENSE_VERSION} v${LIBREALSENSE_VERSION}.tar.gz
 
 # Submodules
 git submodule update --init --recursive
