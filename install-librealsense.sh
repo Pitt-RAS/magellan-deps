018361246dd048ab1be16fbd60d03ee33ad90c1d
Andrew Lobos <andrew@lobos.me>
2018-09-18 18:07:00 -0400

    Fix installer

--- install-librealsense.sh
+++ install-librealsense.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+sudo apt-get install -y software-properties-common
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C8B3A55A6F3EFCDE
 sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo bionic main" -u
-sudo apt-get install librealsense2-dev librealsense2-tools librealsense2-dkms librealsense2-udev-rules
+sudo apt-get install librealsense2-dev librealsense2-tools librealsense2-dkms
